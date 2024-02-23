010cc4ebbf14200ceae213ab8e4bbd26365cd5a0
Caleb Filburn <calebfilburn@gmail.com>
2024-02-23 18:46:02 -0500

    alias config, css reset, init FlashCard component

---
+++ src/lib/components/FlashCard.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  export let front: string;
+  export let back: string;
+
+  function flipCard(card: Element): void {
+    card.classList.toggle('flipped');
+  }
+
+  const cardElements: HTMLCollection = document.getElementsByClassName('card');
+  Array.from(cardElements).forEach((c: Element) => {
+    c.addEventListener('click', () => {
+      flipCard(c);
+    });
+  });
+</script>
+
+<div class="flash-card">
+  <div class="flash-card__front">
+    {front}
+  </div>
+  <div class="flash-card__back">
+    {back}
+  </div>
+</div>
