3225e4f75310477e4dfaa7d74d62a525b42dcad6
Caleb Filburn <calebfilburn@gmail.com>
2024-02-25 22:30:38 -0500

    Proof of concept, add mock local data object

--- src/lib/components/FlashCard.svelte
+++ src/lib/components/FlashCard.svelte
@@ -1,24 +1,64 @@
 <script lang="ts">
-  export let front: string;
-  export let back: string;
+  export let front: string = "Front of the card";
+  export let back: string = "Back of the card";
 
-  function flipCard(card: Element): void {
-    card.classList.toggle('flipped');
+  let isFlipped: boolean = false;
+
+  function handleClick(): void {
+    isFlipped = !isFlipped;
   }
 
-  const cardElements: HTMLCollection = document.getElementsByClassName('card');
-  Array.from(cardElements).forEach((c: Element) => {
-    c.addEventListener('click', () => {
-      flipCard(c);
-    });
-  });
+  function handleKeydown(event: KeyboardEvent): void {
+    if (event.key === "Enter") {
+      isFlipped = !isFlipped;
+    }
+  }
 </script>
 
-<div class="flash-card">
-  <div class="flash-card__front">
-    {front}
-  </div>
-  <div class="flash-card__back">
-    {back}
+<div role="button" tabindex="0" class="flashcard" on:click={handleClick} on:keydown={handleKeydown}>
+  <div class="card" style={`transform: rotateY(${isFlipped ? 180 : 0}deg)`}>
+    <div class="front">
+      {front}
+    </div>
+    <div class="back">
+      {back}
+    </div>
   </div>
 </div>
+
+<style lang="scss">
+  .flashcard {
+    width: 100%;
+    height: 80vh;
+    perspective: 1000px;
+    flex: 0 0 auto;
+    scroll-snap-align: start;
+    margin: 16px auto;
+
+    .card {
+      width: 100%;
+      height: 100%;
+      transform-style: preserve-3d;
+      transition: transform 0.5s;
+
+      .front,
+      .back {
+        position: absolute;
+        width: 100%;
+        height: 100%;
+        backface-visibility: hidden;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 18px;
+        background-color: #f0f0f0;
+        border: 1px solid #ccc;
+        border-radius: 8px;
+      }
+
+      .back {
+        transform: rotateY(180deg);
+      }
+    }
+  }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,55 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import FlashCard from "$lib/components/FlashCard.svelte";
+
+  let cards = [
+    { front: "front 1", back: "back 1" },
+    { front: "front 2", back: "back 2" },
+    { front: "front 3", back: "back 3" },
+    { front: "front 4", back: "back 4" },
+    { front: "front 5", back: "back 5" },
+    { front: "front 6", back: "back 6" },
+    { front: "front 7", back: "back 7" },
+    { front: "front 8", back: "back 8" },
+    { front: "front 9", back: "back 9" },
+    { front: "front 10", back: "back 10" },
+    { front: "front 11", back: "back 11" },
+    { front: "front 12", back: "back 12" },
+    { front: "front 13", back: "back 13" },
+    { front: "front 14", back: "back 14" },
+    { front: "front 15", back: "back 15" },
+    { front: "front 16", back: "back 16" },
+    { front: "front 17", back: "back 17" },
+    { front: "front 18", back: "back 18" },
+    { front: "front 19", back: "back 19" },
+    { front: "front 20", back: "back 20" },
+    { front: "front 21", back: "back 21" },
+    { front: "front 22", back: "back 22" },
+    { front: "front 23", back: "back 23" },
+    { front: "front 24", back: "back 24" },
+    { front: "front 25", back: "back 25" },
+  ];
+</script>
+<h1>Design Flash Cards</h1>
+
+<main>
+  {#each cards as { front, back }, index (index)}
+    <FlashCard {front} {back} />
+  {/each}
+</main>
+
+
+<style lang="scss">
+  main {
+    display: flex;
+    flex-direction: column;
+    height: 90vh;
+    overflow-y: auto;
+    scroll-snap-type: y mandatory;
+    scroll-behavior: smooth;
+    margin: 2rem;
+
+    &::-webkit-scrollbar {
+      display: none;
+    }
+  }
+</style>
